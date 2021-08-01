26718ef1118b87c05445ae45eb077c5151420306
Rumee Ahmed <rumeeahmad@gmail.com>
2021-08-01 18:30:51 +0100

    Created base templated, a homepage and a navigation bar.

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+from views.home import Home
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def home():
-    return 'Hello'
-
-
+app.add_url_rule('/', view_func=Home.as_view('home'))
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ views/home.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from flask.views import MethodView
+
+
+class Home(MethodView):
+    """
+    View that serves the Homepage.
+    """
+    def get(self):
+        return render_template('home.html')
