fdfab7aeb2a674b96abed74f68cfb014489dd411
Rumee Ahmed <rumeeahmad@gmail.com>
2021-08-07 09:47:44 +0100

    created player template and routing logic.

--- views/player.py
+++ views/player.py
@@ -1,10 +1,16 @@
 from flask import render_template
 from flask.views import MethodView
+from models.weezbase import WeezBase
+import random
 
 
 class Players(MethodView):
     """
-    View that serves the Players.
+    View that serves the Players page.
     """
     def get(self):
-        return render_template('players.html')
+        weezbase = WeezBase()
+        player_data = weezbase.db.collection('players').get()
+        players = [player.to_dict() for player in player_data]
+        random.shuffle(players)
+        return render_template('players.html', players=players)
