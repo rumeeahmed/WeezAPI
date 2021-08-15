28ba5353ebae2f7df30c46254f3a5effa632e02a
Rumee Ahmed <rumeeahmad@gmail.com>
2021-08-15 18:09:47 +0100

    Data now being retrieved and processed from the database.

--- views/team_stats.py
+++ views/team_stats.py
@@ -1,13 +1,49 @@
 from flask import render_template
 from flask.views import MethodView
+from models.weezbase import WeezBase
 
 
 class TeamStats(MethodView):
     """
     Object that serves the Total Team Stats page.
     """
-    def get(self):
-        return render_template('team_stats.html')
+    def get(self) -> str:
+        self._initialise_database()
+        return render_template('team_stats.html', labels=self.labels, data=self.data)
+
+    def _initialise_database(self) -> None:
+        """
+        Initialise the database and retrieve the data to be processed passed into the Team_Stats template.
+        :return: None
+        """
+        weezbase = WeezBase()
+        self.labels = []
+        self.data = {
+            'Score': [],
+            'Kills': [],
+            'Deaths': [],
+            'Assists': [],
+            'K/D': [],
+            'Damage': [],
+            'Damage Taken': [],
+            'Headshots': [],
+            'Revives': [],
+            'Teams Wiped': [],
+        }
+
+        documents = weezbase.db.collection('total_team_stats').stream()
+        for document in documents:
+            self.labels.append(document.id)
+            self.data['Score'].append(document.get('team_score'))
+            self.data['Kills'].append(document.get('team_kills'))
+            self.data['Deaths'].append(document.get('team_deaths'))
+            self.data['Assists'].append(document.get('team_assists'))
+            self.data['K/D'].append(document.get('team_kd_average'))
+            self.data['Damage'].append(document.get('team_damage'))
+            self.data['Damage Taken'].append(document.get('team_damage_taken'))
+            self.data['Headshots'].append(document.get('team_headshots'))
+            self.data['Revives'].append(document.get('team_revives'))
+            self.data['Teams Wiped'].append(document.get('team_teams_wiped'))
 
 
 class TeamStatsPerGame(MethodView):
