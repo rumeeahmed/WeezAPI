ffbc1348bc8bea85fcef8003e9da5032679b569b
Rumee Ahmed <rumeeahmad@gmail.com>
2021-08-20 18:47:06 +0100

    removed the replace method that was not required.

--- models/weezbase.py
+++ models/weezbase.py
@@ -22,7 +22,7 @@ class WeezBase:
             firebase_credentials = {
                 "type": os.environ.get('FIREBASE_TYPE'),
                 "project_id": os.environ.get('FIREBASE_PROJECT_ID'),
-                "private_key_id": os.environ.get('FIREBASE_PRIVATE_KEY_ID').replace('\\n', '\n'),
+                "private_key_id": os.environ.get('FIREBASE_PRIVATE_KEY_ID'),
                 "private_key": os.environ.get('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
                 "client_email": os.environ.get('FIREBASE_CLIENT_EMAIL'),
                 "client_id": os.environ.get('FIREBASE_CLIENT_ID'),
