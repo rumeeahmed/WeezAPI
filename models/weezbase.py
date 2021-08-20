69ceb361ed875d92e33d6d2ec2c837494a04cdbd
Rumee Ahmed <rumeeahmad@gmail.com>
2021-08-20 18:46:07 +0100

    updated the firebase connection to use environment variables isntead of a file.

--- models/weezbase.py
+++ models/weezbase.py
@@ -1,5 +1,7 @@
 from firebase_admin import credentials, firestore
+from dotenv import load_dotenv, find_dotenv
 import firebase_admin
+import os
 
 
 class WeezBase:
@@ -16,6 +18,20 @@ class WeezBase:
         :return: None
         """
         if not firebase_admin._apps:
-            cred = credentials.Certificate('/Users/rumeeahmed/Documents/Weez/DataTools/service_key.json')
+            load_dotenv(find_dotenv())
+            firebase_credentials = {
+                "type": os.environ.get('FIREBASE_TYPE'),
+                "project_id": os.environ.get('FIREBASE_PROJECT_ID'),
+                "private_key_id": os.environ.get('FIREBASE_PRIVATE_KEY_ID').replace('\\n', '\n'),
+                "private_key": os.environ.get('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+                "client_email": os.environ.get('FIREBASE_CLIENT_EMAIL'),
+                "client_id": os.environ.get('FIREBASE_CLIENT_ID'),
+                "auth_uri": os.environ.get('FIREBASE_AUTH_URI'),
+                "token_uri": os.environ.get('FIREBASE_TOKEN_URI'),
+                "auth_provider_x509_cert_url": os.environ.get('FIREBASE_AUTH_PROVIDER_URI'),
+                "client_x509_cert_url": os.environ.get('FIREBASE_AUTH_URl'),
+            }
+
+            cred = credentials.Certificate(firebase_credentials)
             firebase_admin.initialize_app(cred)
         self.db = firestore.client()
